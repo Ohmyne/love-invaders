ecf3c81105f0a38aee404b211ccf2b5ccc3a6d81
Myriam Nox <myriam.nox@gmail.com>
2014-05-08 15:25:15 +0200

    ship moves

--- main.lua
+++ main.lua
@@ -1,13 +1,20 @@
+
 function love.keypressed(key)
 	if key == 'escape' then
 		os.exit()
 	end
 end
 
+
 function love.load()
+	shipx = 400
 	ship = love.graphics.newImage("ship.png")
 end
 
 function love.draw()
-    love.graphics.draw(ship, 10, 30)
+    love.graphics.draw(ship, shipx, 550)
+end
+
+function love.update(dt)
+	shipx = shipx + 2
 end
