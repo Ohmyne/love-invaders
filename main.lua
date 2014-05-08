b8443b31af3d63233db03b4df63f59b42e20612c
Myriam Nox <myriam.nox@gmail.com>
2014-05-08 16:50:46 +0200

    refactoring

--- main.lua
+++ main.lua
@@ -10,28 +10,37 @@ function love.load()
 	speed = 180
 	screenwidth = 800
 	shipx = screenwidth / 2
+	shipy = 500
 	shipxmin = 80
-	shipmax = screenwidth - shipxmin
+	shipxmax = screenwidth - shipxmin
 	ship = love.graphics.newImage("ship.png")
 	background = love.graphics.newImage("background.png")
+
 end
 
 function love.draw()
     
     love.graphics.draw(background, 0, 0)	
-    love.graphics.draw(ship, shipx - 16, 500)
+    love.graphics.draw(ship, shipx - 16, shipy)
 end
 
 function love.update(dt)
-	if love.keyboard.isDown ("right") then
+	if love.keyboard.isDown("right") then
 		shipx = shipx + (speed * dt)
 	end
-	if love.keyboard.isDown ("left") then
+	
+	if love.keyboard.isDown("left") then
 		shipx = shipx - (speed * dt)
 	end
-	if shipx > shipmax then
-		shipx = shipmax
+	
+	if love.keyboard.isDown(" ") then
+	
 	end
+	
+	if shipx > shipxmax then
+		shipx = shipxmax
+	end
+	
 	if shipx < shipxmin then
 	   	shipx = shipxmin
 	end
