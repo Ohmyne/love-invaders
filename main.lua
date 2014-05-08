c8a0f47aea57b9913262ab0112e22a42effe3c64
Myriam Nox <myriam.nox@gmail.com>
2014-05-08 15:12:16 +0200

    ship drawn

--- main.lua
+++ main.lua
@@ -4,9 +4,10 @@ function love.keypressed(key)
 	end
 end
 
-function love.draw()
-    love.graphics.print("Hello World", 10, 30)
-	love.graphics.print("Blaba", 30,40)
-	love.graphics.print("hihihi", 100, 30)
+function love.load()
+	ship = love.graphics.newImage("ship.png")
 end
 
+function love.draw()
+    love.graphics.draw(ship, 10, 30)
+end
